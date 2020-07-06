654df4a85693f2316edb9fbc008bd045ffe8a18a
ouko <oukoezzy@gmail.com>
2020-07-06 15:00:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
+import IntervalClassCounter from './components/IntervalClassCounter'
+import DataFetching from './components/DataFetching';
+import ComponentC from './components/ComponentC'
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
+import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
+import ParentComponent from './components/ParentComponent';
+import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HookTimer />
+      <ClassTimer />
+      {/* <FocusInput /> */}
+      {/* <ParentComponent /> */}
+      {/* <DataFetchingOne /> */}
+      {/* <DataFetchingTwo /> */}
+      {/* <CounterOne /> */}
+      {/* <CounterTwo /> */}
+      {/* <CounterThree /> */}
+      {/* <UserContext.Provider value={'Vishwas'}>
+        <ChannelContext.Provider value={'Codevolution'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider> */}
+
+      {/* <DataFetching /> */}
+      {/* <IntervalClassCounter />
+      <IntervalHookCounter /> */}
+      {/* <MouseContainer /> */}
+      {/* <HookMouse /> */}
+      {/* <ClassMouse /> */}
+      {/* <HookCounterFour />  */}
+      {/* <ClassCounterOne /> */}
+      {/* <HookCounterOne /> */}
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+function Counter() {
+    const [counterOne, setCounterOne] = useState(0)
+    const [counterTwo, setCounterTwo] = useState(0)
+
+    const incrementOne = () => {
+        setCounterOne(counterOne + 1)
+    }
+
+    const incrementTwo = () => {
+        setCounterTwo(counterTwo + 1)
+    }
+
+    const isEven = () => {
+        return counterOne % 2 === 0
+    }
+
+    return (
+        <div>
+            <div>
+                <button onClick={incrementOne}>Count One - {counterOne}</button>
+                <span>{isEven() ? 'Even' : 'Odd'}</span>
+            </div>
+            <div>
+                <button onClick={incrementTwo}>Count Two - {counterTwo}</button>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
